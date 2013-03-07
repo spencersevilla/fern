b0e9536ab10edb50f66d94b976f77b512a5bf4ce
Spencer <spencersevilla@gmail.com>
2013-03-06 17:58:13 -0800

    Message.java and Registration.java
    introducing an abstract class to handle Regs and Reqs. This gets passed
    around through the code

---
+++ src/com/spencersevilla/fern/shared/Message.java
@@ -0,0 +1,24 @@
+package com.spencersevilla.fern;
+
+import java.util.*;
+import java.io.*;
+
+public abstract class Message implements Serializable {
+	protected Name name;
+
+	public Message(Name n) {
+		name = n;
+	}
+
+	public Message(String n) {
+		name = new Name(n);
+	}
+
+	public final String toString() {
+		return name.toString();
+	}
+
+	public Name getName() {
+		return new Name(name);
+	}
+}
---
+++ src/com/spencersevilla/fern/shared/Registration.java
@@ -0,0 +1,18 @@
+
+package com.spencersevilla.fern;
+
+import java.util.*;
+import java.io.*;
+
+public class Registration extends Message implements Serializable {
+	private Record record;
+
+	public Registration(Name n, Record r) {
+		super(n);
+		record = new Record(r);
+	}
+
+	public Record getRecord() {
+		return new Record(record);
+	}
+}
