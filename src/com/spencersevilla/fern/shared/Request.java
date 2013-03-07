f53fb97c9793d90a97550b5032cf6758ff8576d7
Spencer <spencersevilla@gmail.com>
2013-03-06 17:58:40 -0800

    move code from Request.java into Message.java

--- src/com/spencersevilla/fern/shared/Request.java
+++ src/com/spencersevilla/fern/shared/Request.java
@@ -3,23 +3,14 @@ package com.spencersevilla.fern;
 import java.util.*;
 import java.io.*;
 
-public class Request implements Serializable {
-	private Name name;
+public class Request extends Message implements Serializable {
 	private int type;
 
-	public final String toString() {
-		return name.toString();
-	}
-
 	public Request(String n) {
-		name = new Name(n);
+		super(n);
 	}
 
 	public Request(Name n) {
-		name = n;
-	}
-
-	public Name getName() {
-		return new Name(name);
+		super(n);
 	}
 }
