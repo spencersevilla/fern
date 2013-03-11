873bfecbac58faa30e8a747aa479a7b46c2ad38f
Spencer <spencersevilla@gmail.com>
2013-03-11 11:31:33 -0700

    changing TestFERN
    now it registers an ICN record

--- utils/TestFERN.java
+++ utils/TestFERN.java
@@ -8,15 +8,25 @@ public class TestFERN {
     public static void main(String[] args) throws Exception {
 		FERNInterface fern = new FERNInterface();
 
-		InetAddress a = InetAddress.getByName("127.0.0.1");
-		byte[] addr = a.getAddress();
+		InetAddress a = InetAddress.getByName("128.114.59.75");
+		String data = new String("XXX");
+		// byte[] addr = a.getAddress();
 
-		System.out.println("Test: Class creation");
-		Name n = new Name("a.b.c");
-		Record r = new Record(n, Type.A, DClass.IN, 0, addr);
-		Service s = new Service(n);
-		Request req = new Request(n);
-		Registration reg = new Registration(n, r);
-		System.out.println("Test Passed!");
+		// System.out.println("Test: Class creation");
+		// Name n = new Name("a.b.c");
+		// Record r = new Record(n, Type.A, DClass.IN, 0, addr);
+		// Service s = new Service(n);
+		// Request req = new Request(n);
+		// Registration reg = new Registration(n, r);
+		// System.out.println("Test Passed!");
+
+		Name n = new Name("icn");
+		Record r = new Record(null, Type.A, DClass.IN, 0, a);
+		Record r2 = new Record(null, Type.ICN, DClass.IN, 0, data.getBytes());
+		Service serv = new Service(n);
+		serv.addRecord(r);
+		serv.addRecord(r2);
+
+		fern.registerService(serv);
 	}
 }
